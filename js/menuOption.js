1c1d6651492c12944a3f59ba61073b4ab2dbb555
ggajjar <36022745+ggajjar@users.noreply.github.com>
2018-12-05 17:19:06 -0500

    Add files via upload

---
+++ js/menuOption.js
@@ -0,0 +1,59 @@
+function openPosts()
+{	
+	window.location = "Posts.html";
+}
+
+function openNotification()
+{
+	window.location = "Notification.html";
+}
+
+function openGrades()
+{
+	window.location = "Grades.html";
+}
+
+function openCalender()
+{
+	window.location = "Calender.html";
+}
+
+function openPeople()
+{
+	window.location = "People.html";
+}
+
+function openGeneral()
+{
+	window.location = "General.html";
+}
+
+function openHistory()
+{
+	window.location = "History.html";
+}
+
+function openSettings()
+{
+	window.location = "Settings.html";
+}
+
+function openMessages()
+{
+	window.location = "Messages.html"
+}
+
+function openCourses()
+{
+	window.location = "Courses.html";
+}
+
+function openHome()
+{
+	window.location = "Home.html";
+}
+
+function openLogin()
+{
+	window.location = "Login.html";
+}
