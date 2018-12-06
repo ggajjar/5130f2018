83f327b917e66f7d53125d079380e01211c22aa5
ggajjar <36022745+ggajjar@users.noreply.github.com>
2018-12-05 23:15:22 -0500

    Create menuOptions.js

---
+++ Week-7/Admin/js/menuOptions.js
@@ -0,0 +1,59 @@
+function openPosts()
+{	
+	window.location = "Posts.html";
+}
+
+function openNotification()
+{
+	window.location = "AdminNotifications.html";
+}
+
+function openSchedule()
+{
+	window.location = "AdminSchedule.html";
+}
+
+function openCalender()
+{
+	window.location = "AdminCalender.html";
+}
+
+/*function openPeople()
+{
+	window.location = "People.html";
+}
+
+function openGeneral()
+{
+	window.location = "General.html";
+}
+
+function openHistory()
+{
+	window.location = "History.html";
+}*/
+
+function openSettings()
+{
+	window.location = "AdminSettings.html";
+}
+
+function openMessages()
+{
+	window.location = "AdminMessages.html"
+}
+
+function openCourses()
+{
+	window.location = "AdminCourses.html";
+}
+
+function openHome()
+{
+	window.location = "AdminHome.html";
+}
+
+function openLogin()
+{
+	window.location = "../index.html";
+}
